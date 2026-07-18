8a2b0be1d5d5668ba392c8578b2e9de26b97cbfc
Zeynal <zeynalzeynalov888@gmail.com>
2026-07-18 17:45:03 +0400

    Add mapping for updating Member entity from request

--- src/main/java/com/devjoint/librarymanagersystem/mapper/MemberMapper.java
+++ src/main/java/com/devjoint/librarymanagersystem/mapper/MemberMapper.java
@@ -4,14 +4,20 @@ import com.devjoint.librarymanagersystem.model.dto.request.MemberRequest;
 import com.devjoint.librarymanagersystem.model.dto.response.MemberResponse;
 import com.devjoint.librarymanagersystem.model.entity.Member;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
+
     MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     Member toEntity(MemberRequest memberRequest);
+
     MemberResponse toResponse(Member member);
+
+    @Mapping(target = "id", ignore = true)
     void updateEntityFromRequest(MemberRequest memberRequest, @MappingTarget Member member);
 }
